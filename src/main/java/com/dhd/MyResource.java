1c7115f84bd0197993142d46f666ac06da7a416c
Peter Gifford <petergifford@peters-mbp.home>
2019-01-23 22:45:05 -0700

    init commit

---
+++ src/main/java/com/dhd/MyResource.java
@@ -0,0 +1,17 @@
+package com.dhd;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("myresource")
+public class MyResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+}
