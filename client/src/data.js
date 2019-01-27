3cfac3cc8f05a0cddbde0536cb40d0c44148474b
Peter Gifford <peter.lee.gifford@gmail.com>
2019-01-27 16:48:12 -0700

    add files

---
+++ client/src/data.js
@@ -0,0 +1,6 @@
+const data = [
+  { _id: 1, author: 'Bryan', text: 'Wow this is neat', updatedAt: new Date(), createdAt: new Date() },
+  { _id: 2, author: 'You', text: 'You\'re __right!__', updatedAt: new Date(), createdAt: new Date() },
+];
+
+export default data;
