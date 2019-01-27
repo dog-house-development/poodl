f7476409efe98c603a5883fabf15757ab6677659
Peter Gifford <peter.lee.gifford@gmail.com>
2019-01-27 16:10:08 -0700

    Test

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,23 @@ router.put('/comments/:commentId', (req, res) => {
   });
 });
 
+router.put('/comments/:commentId', (req, res) => {
+  const { commentId } = req.params;
+  if (!commentId) {
+    return res.json({ success: false, error: 'No comment id provided' });
+  }
+  Comment.findById(commentId, (error, comment) => {
+    if (error) return res.json({ success: false, error });
+    const { author, text } = req.body;
+    if (author) comment.author = author;
+    if (text) comment.text = text;
+    comment.save(error => {
+      if (error) return res.json({ success: false, error });
+      return res.json({ success: true });
+    });
+  });
+});
+
 router.delete('/comments/:commentId', (req, res) => {
   const { commentId } = req.params;
   if (!commentId) {
