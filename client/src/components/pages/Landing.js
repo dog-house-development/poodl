7c2fc7a6048d6eaa3ad86d794affe444cb8094f0
Samuel Behrens <sbehrens1414@gmail.com>
2019-01-31 20:33:05 -0700

    Remove links from landing page and update scripts

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 class Landing extends Component {
   render() {
@@ -7,12 +6,6 @@ class Landing extends Component {
       <div>
         <h4>Welcome to Poodl!</h4>
         <p>The member management tool for rural senior centers!</p>
-        <div>
-          <Link to="/register">Register</Link>
-        </div>
-        <div>
-          <Link to="/login">Log In</Link>
-        </div>
       </div>
     );
   }
