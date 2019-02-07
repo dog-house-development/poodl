0ffa208e48cbdf6a4899fb54f5da4969c88343ad
timothyosen <timothy.osen@gmail.com>
2019-02-06 22:35:28 -0700

    Added get to Members.js

--- routes/api/members.js
+++ routes/api/members.js
@@ -8,6 +8,15 @@ const passport = require("passport");
 // Load Member model
 const Member = require("../../models/Member");
 
+// @route GET api/Member/get
+// should return all Members
+router.get("/get", (req, res) => {
+  Member.find((err, members) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: members });
+  });
+});
+
 router.post("/register", (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
