8cc6966e91008550553a089c91e088eb56a692fd
Peter Gifford <peter.lee.gifford@gmail.com>
2019-02-10 16:15:18 -0700

    try catches added

--- test/admin_tests.js
+++ test/admin_tests.js
@@ -27,67 +27,65 @@ before(function(done) {
 
 describe('/GET admins', () => {
     it('it should get all the admins', done => {
-        try {
-            chai.request(server)
-                .get('/api/admins/get')
-                .end((err, res) => {
+        chai.request(server)
+            .get('/api/admins/get')
+            .then((err, res) => {
+                try {
                     res.should.have.status(200);
                     res.body.should.have.property('success').eql(true);
                     //res.body.should.have.property("data"); //should go deeper having issues add later
                     //res.body.length.should.be.eql(0);
-                    done();
-                });
-        } catch (e) {
-            done(e);
-        }
+                } catch (err) {}
+            })
+            .catch(err => {
+                throw err;
+            });
+        done();
     });
 }).timeout(120000);
 
 describe('/REGISTER admins', () => {
     it('it should create a new admin', done => {
-        try {
-            let admin = {
-                name: 'testy boy',
-                email: 'test@gmail.com',
-                password: 'greatpassword1!@',
-                password2: 'greatpassword1!@'
-            };
+        let admin = {
+            name: 'testy boy',
+            email: 'test@gmail.com',
+            password: 'greatpassword1!@',
+            password2: 'greatpassword1!@'
+        };
 
-            chai.request(server)
-                .post('/api/admins/register')
-                .send(admin)
-                .end((err, res) => {
+        chai.request(server)
+            .post('/api/admins/register')
+            .send(admin)
+            .then((err, res) => {
+                try {
                     res.should.have.status(200);
                     res.body.should.have.property('name').eql('testy boy');
                     res.body.should.have.property('email').eql('test@gmail.com');
-
-                    done();
-                });
-        } catch (e) {
-            done(e);
-        }
+                } catch (err) {}
+            })
+            .catch(err => {
+                throw err;
+            });
+        done();
     });
 }).timeout(120000);
 
 describe('/LOGIN admins', () => {
     it('it should login an existing admin', done => {
-        try {
-            let loginInfo = {
-                email: 'test@gmail.com',
-                password: 'greatpassword1!@'
-            };
-            chai.request(server)
-                .post('/api/admins/login')
-                .send(loginInfo)
-                .end((err, res) => {
+        let loginInfo = {
+            email: 'test@gmail.com',
+            password: 'greatpassword1!@'
+        };
+        chai.request(server)
+            .post('/api/admins/login')
+            .send(loginInfo)
+            .end((err, res) => {
+                try {
                     res.should.have.status(200);
                     res.body.should.have.property('success').eql(true);
                     res.body.should.have.property('token');
-
-                    done();
-                });
-        } catch (e) {
-            done(e);
-        }
+                } catch (err) {}
+            });
+        done();
     }).timeout(120000);
 });
