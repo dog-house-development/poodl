5abb831d0d39a470f762e704604855d65f756572
Karl Molina <karlgmolina@gmail.com>
2019-02-14 20:13:01 -0700

    Add volunteers reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import adminsReducer from './adminsReducer';
+import volunteersReducer from './volunteersReducer';
 import errorReducer from './errorReducer';
 
 export default combineReducers({
     auth: authReducer,
     admins: adminsReducer,
+    volunteers: volunteersReducer,
     errors: errorReducer
 });
