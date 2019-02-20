9f1530347836398e9e859235c8d09ff261f12d9b
Peter Gifford <peter.lee.gifford@gmail.com>
2019-02-20 10:14:54 -0700

    Added timestamps

--- models/Service.js
+++ models/Service.js
@@ -1,42 +1,45 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ServiceSchema = new Schema({
-    name: {
-        type: String,
-        required: true
+const ServiceSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        times: [
+            {
+                type: String,
+                required: true
+            }
+        ],
+        dates: [
+            {
+                type: String,
+                required: true
+            }
+        ],
+        //The plan is to have admin and volunteer list be IDs
+        admins: [
+            {
+                type: String
+            }
+        ],
+        volunteers: [
+            {
+                type: String
+            }
+        ],
+        members: [
+            {
+                type: String
+            }
+        ],
+        seniorCenter: {
+            type: String,
+            required: true
+        }
     },
-    times: [
-        {
-            type: String,
-            required: true
-        }
-    ],
-    dates: [
-        {
-            type: String,
-            required: true
-        }
-    ],
-    //The plan is to have admin and volunteer list be IDs
-    admins: [
-        {
-            type: String
-        }
-    ],
-    volunteers: [
-        {
-            type: String
-        }
-    ],
-    members: [
-        {
-            type: String
-        }
-    ],
-    seniorCenter: {
-        type: String,
-        required: true
-    }
-});
+    { timestamps: true }
+);
 module.exports = mongoose.model('services', ServiceSchema);
