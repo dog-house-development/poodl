36bd6e070ef4cdf3d2d2ae06a1cce6e33fb70185
Tim Osen <timothy.osen@gmail.com>
2019-02-26 17:10:09 -0700

    Shell of test for profile  member page  when finished

---
+++ client/src/components/pages/views/tests/MemberProfile.test.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import _ from 'lodash';
+import { Link } from 'react-router-dom';
+import { MemberProfile, mapStateToProps, mapDispatchToProps } from '../MemberProfile';
+
+configure({ adapter: new Adapter() });
+
+const match = {
+    params: {
+        _id: '123' //any id you want to set
+    }
+};
+describe('MemberProfile tests', () => {
+    let state, props, wrapper, instance;
+    const setInstanceAndWrapper = (_props = {}, _state = {}) => {
+        state = _.assign(
+            {},
+            {
+                members: {
+                    one: [
+                        {
+                            _id: '123',
+                            firstName: 'Big',
+                            lastName: 'Tup',
+                            membershipDate: '02/20/2020',
+                            email: 'bigtup@nowhere.com'
+                        }
+                    ]
+                },
+                errors: {}
+            },
+            _state
+        );
+        props = _.assign({}, _props);
+        wrapper = shallow(
+            <MemberProfile
+                match={match}
+                {..._.assign(
+                    {},
+                    props,
+                    mapStateToProps(state, props),
+                    mapDispatchToProps(jasmine.createSpy('dispatch'))
+                )}
+            />
+        );
+        instance = wrapper.instance();
+    };
+
+    beforeEach(() => {
+        setInstanceAndWrapper();
+    });
+
+    describe('mapStateToProps', () => {
+        it('should map state to props', () => {
+            expect(mapStateToProps(state, props)).toEqual({
+                member: [
+                    {
+                        _id: '123',
+                        firstName: 'Big',
+                        lastName: 'Tup',
+                        membershipDate: '02/20/2020',
+                        email: 'bigtup@nowhere.com'
+                    }
+                ],
+                errors: {}
+            });
+        });
+    });
+
+    describe('mapDispatchToProps', () => {
+        it('should map dispatch to props', () => {
+            const dispatch = jest.fn();
+            expect(JSON.stringify(mapDispatchToProps(dispatch))).toEqual(JSON.stringify({ getMembers: () => {} }));
+        });
+    });
+
+    describe('componentDidMount', () => {
+        it('should run without breaking', () => {
+            spyOn(instance, 'componentDidMount');
+            instance.componentDidMount();
+        });
+    });
+
+    describe('render', () => {
+        it('should render correctly', () => {
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+});
