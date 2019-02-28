5dc95a67b7d3215cbba1fc11d04958c35edc8ee9
Tim Osen <timothy.osen@gmail.com>
2019-02-28 16:56:43 -0700

    add to member_tests

--- test/member_test.js
+++ test/member_test.js
@@ -30,7 +30,7 @@ after(function() {
 
 describe('Members suite /ADD,/GET,/GET/:ID,/EDIT/:ID, /EDIT,/DELETE', () => {
     it('it should add a new member', done => {
-        let testMember = {
+        let testMember1 = {
             firstName: 'Charles',
             lastName: 'Boile',
             email: 'SpecialDiet2@gmail.com',
@@ -38,6 +38,17 @@ describe('Members suite /ADD,/GET,/GET/:ID,/EDIT/:ID, /EDIT,/DELETE', () => {
             seniorCenter: 'Nice little place',
             renewalDate: 'Today',
             specialDiet: ['fish', 'chicken nuggets']
+        };
+
+        let testMember2 = {
+            firstName: 'Karen',
+            lastName: 'Engel',
+            email: 'KarenEngel@gmail.com',
+            address: '123 4th Avenue',
+            seniorCenter: 'Highgate',
+            membershipDate: 'Frever',
+            renewalDate: '02-03-2020',
+            specialDiet: ['beef', 'chicken nuggets']
         };
 
         chai.request(server)
@@ -56,7 +67,32 @@ describe('Members suite /ADD,/GET,/GET/:ID,/EDIT/:ID, /EDIT,/DELETE', () => {
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.have.property('success').eql(true);
-                tempId = res.body.data[0]._id;
+                res.body.should.have.property('data');
+                member1ID = res.body.data[1]._id;
+                member2ID = res.body[0]._id;
+
+                res.body.data[1].should.have.property('_id');
+                res.body.data[1].should.have.property('firstName').eql('Charles');
+                res.body.data[1].should.have.property('lastName').eql('Boile');
+                res.body.data[1].should.have.property('email').eql('SpecialDiet2@gmail.com');
+                res.body.data[1].should.have.property('address').eql('Brooklyn 99');
+                res.body.data[1].should.have.property('seniorCenter').eql('Nice little place');
+                res.body.data[1].should.have.property('membershipDate').eql('Frever');
+                res.body.data[1].should.have.property('renewalDate').eql('Today');
+                res.body.data[1].should.have.property('specialDiet').eql(['fish', 'chicken nuggets']);
+                res.body.data[0].should.have.property('date');
+
+                res.body.data[1].should.have.property('_id');
+                res.body.data[1].should.have.property('firstName').eql('Karen');
+                res.body.data[1].should.have.property('lastName').eql('Engel');
+                res.body.data[1].should.have.property('email').eql('KarenEngel@gmail.com');
+                res.body.data[1].should.have.property('address').eql('123 4th Avenue');
+                res.body.data[1].should.have.property('seniorCenter').eql('Highgate');
+                res.body.data[1].should.have.property('membershipDate').eql('Frever');
+                res.body.data[1].should.have.property('renewalDate').eql('02-03-2020');
+                res.body.data[1].should.have.property('specialDiet').eql(['beef', 'chicken nuggets']);
+                res.body.data[0].should.have.property('date');
+
                 done();
             });
     });
