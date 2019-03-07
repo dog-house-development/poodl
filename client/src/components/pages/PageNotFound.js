71c7a43289932dd2d26fcb25a236b8ac74ee6abb
Karl Molina <karlgmolina@gmail.com>
2019-03-06 22:33:27 -0700

    Fix 404 page styling

--- client/src/components/pages/PageNotFound.js
+++ client/src/components/pages/PageNotFound.js
@@ -4,19 +4,17 @@ import GoHome from '../ui/GoHome';
 class PageNotFound extends Component {
     render() {
         return (
-            <div className="landing-container">
-                <div className="landing-header">
-                    <h1 className="landing-title">404</h1>
-                    <h1>Page not found</h1>
-                    <p className="landing-info">
-                        If you believe this occured in error, please contact support at{' '}
-                        <a href="mailto:doghousedevelop@gmail.com" target="_blank" rel="noopener noreferrer">
-                            doghousedevelop@gmail.com.
-                        </a>
-                    </p>
-                    <h3 className="landing-info">Thanks</h3>
-                    <GoHome buttonOrLink="button" size="large" />
-                </div>
+            <div className="page-not-found">
+                <h1 className="">404</h1>
+                <h2>Page not found</h2>
+                <p className="">
+                    If you believe this occured in error, please contact support at{' '}
+                    <a href="mailto:doghousedevelop@gmail.com" target="_blank" rel="noopener noreferrer">
+                        doghousedevelop@gmail.com.
+                    </a>
+                </p>
+                <h3 className="">Thanks</h3>
+                <GoHome buttonOrLink="button" size="large" />
             </div>
         );
     }
