77be0b99cb1b71d2b6aeab8088ba0a31dfa10d4a
Tim Osen <timothy.osen@gmail.com>
2019-03-28 17:38:26 -0600

    Add Edit Admin. Display only Super and Admin accessLevel on view all

---
+++ client/src/components/pages/admins/adminFields.js
@@ -0,0 +1,39 @@
+export const Categories = {
+    admin: { id: 'admin', title: 'Admin Info' }
+};
+
+const memberFields = [
+    {
+        id: 'firstName',
+        type: 'text',
+        label: 'First name',
+        category: Categories.admin.id
+    },
+    {
+        id: 'lastName',
+        type: 'text',
+        label: 'Last name',
+        category: Categories.admin.id
+    },
+    {
+        id: 'email',
+        type: 'email',
+        label: 'Email',
+        category: Categories.admin.id
+    },
+    {
+        id: 'seniorCenterId',
+        type: 'text',
+        label: 'Senior  Center ID',
+        category: Categories.admin.id
+    },
+    {
+        id: 'accessLevel',
+        type: 'radio',
+        label: 'Access Level',
+        options: ['Super', 'Admin', 'Volunteer'],
+        category: Categories.admin.id
+    }
+];
+
+export default memberFields;
