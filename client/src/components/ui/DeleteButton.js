541d4b74056ee437cc4f70b3990c5fe775297ee3
Sam Behrens <sbehrens1414@gmail.com>
2019-04-30 19:30:47 -0600

    Fix path

--- client/src/components/ui/DeleteButton.js
+++ client/src/components/ui/DeleteButton.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
-import ConfirmModal from './modals/ConfirmModal';
+import ConfirmModal from './modal/ConfirmModal';
 
 const propTypes = {
     onConfirm: PropTypes.func.isRequired,
